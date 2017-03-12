670c25c93b0a4a297f97f721cd590c6994acff9a
anupamsarkar54 <AnuPiyaNil@Anupams-MacBook-Air.local>
2017-03-12 18:14:19 -0400

    new change
    new change to xml and Doaimpl

--- src/main/java/com/student/dao/StudentDaoImpl.java
+++ src/main/java/com/student/dao/StudentDaoImpl.java
@@ -9,20 +9,15 @@ import org.springframework.stereotype.Repository;
 import com.student.model.Student;
 @Repository
 public class StudentDaoImpl implements StudentDao{
+
 	@Autowired
-	DataSource datasource;
 	JdbcTemplate jdbcTemplate;
-	@Autowired
-	public  StudentDaoImpl(DataSource datasource) {
-		this.datasource = datasource;
-		//jdbcTemplate = new JdbcTemplate(this.datasource);
-	}
+	
 	
 	
 	@Override
 	public String addStudent(Student std) {
 		 	String sql = "insert into Student (name, age) values (?, ?)";
-		 	jdbcTemplate = new JdbcTemplate(this.datasource);
 		 	int result = jdbcTemplate.update(sql, std.getName(),12);
 		 
 		    System.out.println("Created Record Name = " + std.getName() + " Age = " + result);
@@ -32,7 +27,7 @@ public class StudentDaoImpl implements StudentDao{
 	@Override
 	public Student getStudent(int id) {
 		String sql = "Select * from Student where id=?";
-	 	jdbcTemplate = new JdbcTemplate(this.datasource);
+	 	
 	 	Student student = jdbcTemplate.queryForObject(sql, new Object[]{id},new StudentMapper());
 		return student;
 	}
